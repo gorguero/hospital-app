6a9167fea11063de3201f2077861a74424604ee4
gorguero <jcaguerodev@gmail.com>
2024-01-30 17:11:50 -0300

    Rutas hijas para Pages

--- adminpro/src/app/app-routing.module.ts
+++ adminpro/src/app/app-routing.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+import { PagesRoutingModule } from './pages/pages.routing';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
-import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
+  
 
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
@@ -30,7 +20,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
   ],
   exports: [RouterModule]
 })
