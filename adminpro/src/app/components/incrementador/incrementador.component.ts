83580056a8bd7128a07e4a1d6e6e4ac920e34156
gorguero <jcaguerodev@gmail.com>
2024-02-01 17:25:19 -0300

    Creando componente incrementador

---
+++ adminpro/src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styles: [
+  ]
+})
+export class IncrementadorComponent {
+
+  progreso: number = 50;
+
+  get getProgreso(){
+    return `${this.progreso}%`;
+  }
+
+  cambiarValor( valor:number ){
+
+    if( this.progreso >= 100 && valor >=0 ){
+      return this.progreso = 100;
+    }
+
+    if( this.progreso <= 0 && valor < 0 ){
+      return this.progreso = 0;
+    }
+
+    return this.progreso = this.progreso + valor;
+
+  }
+
+}
--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -9,6 +9,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+import { ComponentsModule } from '../components/components.module';
 
 
 
@@ -23,7 +24,8 @@ import { PagesComponent } from './pages.component';
     CommonModule,
     SharedModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    ComponentsModule
   ],
   exports: [
     DashboardComponent,
