1190448678c7c9ac97acb0ea897d402192199d36
gorguero <jcaguerodev@gmail.com>
2024-02-03 19:04:51 -0300

    Servicio Settings

--- adminpro/src/app/pages/pages.component.ts
+++ adminpro/src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-pages',
@@ -8,13 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit{
 
-  //./assets/css/colors/default-dark.css
-
-  public linkTheme = document.querySelector('#theme');
+  constructor( private settingService:SettingsService ){}
   
   ngOnInit(): void {
-    const url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
-    this.linkTheme?.setAttribute('href', url);
+    
   }
 
 
