4807972ada37ff9c5ff48c11944e2788d3bbddf8
gorguero <jcaguerodev@gmail.com>
2024-02-04 21:01:44 -0300

    Uso de scripts para archivos importados

--- adminpro/src/app/pages/pages.component.ts
+++ adminpro/src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunctions():void;
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -12,7 +14,7 @@ export class PagesComponent implements OnInit{
   constructor( private settingService:SettingsService ){}
   
   ngOnInit(): void {
-    
+    customInitFunctions();
   }
 
 
