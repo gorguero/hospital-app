013920421a93fb3d64a1f92126f7c0be0d5e7de2
gorguero <jcaguerodev@gmail.com>
2024-02-19 18:35:45 -0300

    Login de usuario

--- adminpro/src/app/auth/login/login.component.ts
+++ adminpro/src/app/auth/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginForm } from 'src/app/interfaces/login-form.interface';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +12,36 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(private router:Router) {
+  public formSubmitted = false;
+
+  public loginForm = this.fb.group({
+    email: [ 'joao@gmail.com', [Validators.required, Validators.email] ],
+    password: [ '123456', Validators.required ],
+    remember: [false]
+  });
+
+  constructor(
+    private router:Router, 
+    private fb: FormBuilder,
+    private usuarioService:UsuarioService
+    ) {
     
   }
 
   login(){
-    this.router.navigateByUrl('/')
+
+    // console.log(this.loginForm.value);
+    this.usuarioService.login( this.loginForm.value as LoginForm )
+      .subscribe({
+        next: resp => {
+          console.log(resp);          
+        },
+        error: err => {
+          Swal.fire('Error', err.error.msg, 'error');
+        }
+      })
+
+    // this.router.navigateByUrl('/')
   }
 
 }
--- adminpro/src/app/services/usuario.service.ts
+++ adminpro/src/app/services/usuario.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { environment } from 'src/environments/environment';
+import { LoginForm } from '../interfaces/login-form.interface';
 
 const base_url = environment.base_url;
 
@@ -13,9 +14,11 @@ export class UsuarioService {
   constructor( private http: HttpClient ) { }
 
   registrarUsuario( formData: RegisterForm ){
-
     return this.http.post(`${base_url}/usuarios`, formData);
+  }
 
+  login( formData: LoginForm ){
+    return this.http.post(`${base_url}/login`, formData);
   }
 
 }
