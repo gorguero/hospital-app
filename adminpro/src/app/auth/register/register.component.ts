771ec8024666da00033d25d575c44c3bf4789ecc
gorguero <jcaguerodev@gmail.com>
2024-02-19 15:57:58 -0300

    Formulario de Registro

--- adminpro/src/app/auth/register/register.component.ts
+++ adminpro/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  public registerForm = this.fb.group({
+    nombre: [ 'Joao', [Validators.required, Validators.min(3)] ],
+    email: [ 'testpro@gmail.com', [Validators.required, Validators.email] ],
+    password: [ '123456789', Validators.required ],
+    password2: [ '123456789', Validators.required ],
+    terminos: [ false, Validators.required ]
+  });
+
+  constructor( private fb: FormBuilder ){}
+
+  registrarUsuario(){
+    console.log(this.registerForm.value)
+  }
 
 }
