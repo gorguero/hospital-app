12a725d99f68494961682462b2f06275f8901f9a
gorguero <jcaguerodev@gmail.com>
2024-02-20 13:37:06 -0300

    Guardando info en local storage

--- adminpro/src/app/services/usuario.service.ts
+++ adminpro/src/app/services/usuario.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { environment } from 'src/environments/environment';
 import { LoginForm } from '../interfaces/login-form.interface';
@@ -14,11 +16,21 @@ export class UsuarioService {
   constructor( private http: HttpClient ) { }
 
   registrarUsuario( formData: RegisterForm ){
-    return this.http.post(`${base_url}/usuarios`, formData);
+    return this.http.post(`${base_url}/usuarios`, formData)
+      .pipe(
+        tap( (resp: any) => {
+          localStorage.setItem('token', resp.token)
+        } )
+      )
   }
 
   login( formData: LoginForm ){
-    return this.http.post(`${base_url}/login`, formData);
+    return this.http.post(`${base_url}/login`, formData)
+      .pipe(
+        tap( (resp: any) => {
+          localStorage.setItem('token', resp.token)
+        } )
+      )
   }
 
 }
