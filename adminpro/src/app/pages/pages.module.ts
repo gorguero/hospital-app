f60557e4e5a104c37fa66078e08a2e153e6def20
gorguero <jcaguerodev@gmail.com>
2024-02-29 17:31:02 -0300

    Creación de componente MI Perfil de Usuario

--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -14,6 +14,7 @@ import { ComponentsModule } from '../components/components.module';
 import { AccountSettingComponent } from './account-setting/account-setting.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 
 
@@ -26,6 +27,7 @@ import { RxjsComponent } from './rxjs/rxjs.component';
     AccountSettingComponent,
     PromesasComponent,
     RxjsComponent,
+    PerfilComponent,
   ],
   imports: [
     CommonModule,
