0f9d4792a514bd2c85b87da16a3d5e4cadede070
gorguero <jcaguerodev@gmail.com>
2024-04-10 16:31:50 -0300

    Mostrando información de la búsqueda

--- adminpro/src/app/pages/busqueda/busqueda.component.ts
+++ adminpro/src/app/pages/busqueda/busqueda.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Hospital } from 'src/app/models/hospital.model';
+import { Medico } from 'src/app/models/medico.model';
+import { Usuario } from 'src/app/models/usuario.model';
+import { BusquedasService } from 'src/app/services/busquedas.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,18 +12,35 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BusquedaComponent implements OnInit{
 
-  constructor(private activatedRoute:ActivatedRoute){}
+  public usuarios:Usuario[] = [];
+  public medicos:Medico[] = [];
+  public hospitales:Hospital[] = [];
+
+  constructor(
+    private activatedRoute:ActivatedRoute,
+    private busquedasService:BusquedasService
+  ){}
 
   ngOnInit(): void {
     
     this.activatedRoute.params.subscribe(
-      ({termino}) => {
-        console.log(termino)
-      }
+      ({termino}) => this.busquedaGlobal(termino)
     )
 
   }
-
   
+  busquedaGlobal(termino:string){
+    this.busquedasService.busquedaGlobal(termino)
+      .subscribe( (resp:any) => {
+        console.log(resp)
+        this.usuarios   = resp.usuarios;
+        this.medicos    = resp.medicos;
+        this.hospitales = resp.hospitales;
+      })
+  }
+
+  abrirMedico(medico:Medico){
+    
+  }
 
 }
